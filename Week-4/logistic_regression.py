21e6766313e24fdf67ef51f653eaeca601e3d2a2
lunde chen <lunde.chen@yahoo.fr>
2021-04-22 14:37:54 +0800

    week 4

---
+++ Week-4/logistic_regression.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+class LogisticRegressionGradientDescent:
+    def __init__(self, learning_rate=0.001, n_iters=1000):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        # init parameters
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # gradient descent
+        for _ in range(self.n_iters):
+            # approximate y with linear combination of weights and x, plus bias
+            linear_model = np.dot(X, self.weights) + self.bias
+            # apply sigmoid function
+            y_predicted = self._sigmoid(linear_model)
+
+            # compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+            # update parameters
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self._sigmoid(linear_model)
+        y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
+        return np.array(y_predicted_cls)
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+
+class LogisticRegressionNewtonRaphson:
+    def __init__(self, n_iters=1000):
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        # init parameters
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # gradient descent
+        for _ in range(self.n_iters):
+            # approximate y with linear combination of weights and x, plus bias
+            linear_model = np.dot(X, self.weights) + self.bias
+            # apply sigmoid function
+            y_predicted = self._sigmoid(linear_model)
+
+            # compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+            # update parameters
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self._sigmoid(linear_model)
+        y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
+        return np.array(y_predicted_cls)
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def gradient(self, x, y, th):
+        xTrans = x.transpose()
+        sig = self._sigmoid(np.dot(x, th))
+        grad = np.dot(xTrans, (sig - y))
+        grad = grad / len(x)  # len: number of feature rows
+        return grad
+
+    def hessian(self, x, y, th):
+        xTrans = x.transpose()
+        sig = self._sigmoid(np.dot(x, th))
+        result = (1.0 / len(x) * np.dot(xTrans, x) * np.diag(sig) * np.diag(1 - sig))
+        return result
+
+    def updateTh(self, x, y, th):
+        hessianInv = np.linalg.inv(self._hessian(x, y, th))
+        grad = self._gradient(x, y, th)
+        th = th - np.dot(hessianInv, grad)
+        return th
+
+
---
+++ Week-4/logistic_regression_tests.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import sklearn
+
+from logistic_regression import LogisticRegressionGradientDescent
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+heart = pd.read_csv("SAheart.data")
+heart.famhist.replace(to_replace=['Present', 'Absent'], value=[1, 0], inplace=True)
+heart.drop(['row.names'], axis=1, inplace=True)
+X = heart.iloc[:, :-1]
+y = heart.iloc[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+regressor = LogisticRegressionGradientDescent(learning_rate=0.0001, n_iters=1000)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+
+perf = sklearn.metrics.confusion_matrix(y_test, y_pred)
+print("LR classification perf:\n", perf)
+
+error_rate = np.mean(y_test != y_pred)
+print("LR classification error rate:\n", error_rate)

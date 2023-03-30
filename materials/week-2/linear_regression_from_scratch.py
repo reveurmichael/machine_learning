4341c7bfa6d638683c1dc44aa4a276bf818a412e
Lunde Chen <lunde.chen@outlook.com>
2023-03-30 16:15:44 +0800

    v

--- materials/week-2/linear_regression_from_scratch.py
+++ materials/week-2/linear_regression_from_scratch.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 class LinearRegression:
     def __init__(self, learning_rate=0.0001, n_iters=30000):
@@ -29,3 +31,24 @@ class LinearRegression:
     def predict(self, X):
         y_predicted = np.dot(X, self.weights) + self.bias
         return y_predicted
+
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+

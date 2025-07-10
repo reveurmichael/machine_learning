49e17f741656899a9ebcc7b998a28bec9a616ca2
lunde chen <lunde.chen@outlook.com>
2025-07-10 21:52:18 +0800

    .

--- llm-play-snake-game-v4-Extensions/extensions/fine-tune-v0.01/finetune_snake_qlora.py
+++ llm-play-snake-game-v4-Extensions/extensions/fine-tune-v0.01/finetune_snake_qlora.py
@@ -7,11 +7,12 @@
 
 
 # 512 tokens, 1000 games
-# python finetune_snake_qlora.py --model gemma2-9b --data /home/utseus22/machine_learning/llm-play-snake-game-v4-Extensions/logs/extensions/datasets/grid-size-10/heuristics_v0.04_20250710_192650/bfs-512/BFS-512_dataset.jsonl --max_length 512
+# python finetune_snake_qlora.py --model gemma2-9b --data /home/utseus22/machine_learning/llm-play-snake-game-v4-Extensions/logs/extensions/datasets/grid-size-10/heuristics_v0.04_20250710_212824/bfs-512/BFS-512_dataset.jsonl --max_length 512
 
 
-# 512 tokens, 100 games
+# 512 tokens, 1000 games
 # python finetune_snake_qlora.py --model gemma2-9b --data /home/utseus22/machine_learning/llm-play-snake-game-v4-Extensions/logs/extensions/datasets/grid-size-10/heuristics_v0.04_20250710_192650/bfs-512/BFS-512_dataset.jsonl --max_length 512
+# python finetune_snake_qlora.py --model gemma-3-4b-it --data /home/utseus22/machine_learning/llm-play-snake-game-v4-Extensions/logs/extensions/datasets/grid-size-10/heuristics_v0.04_20250710_192650/bfs-512/BFS-512_dataset.jsonl --max_length 512
 
 
 
